f7089122df73a18ff5a6f3725aaccd234daeced2
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-09-15 19:25:29 +0300

    create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(".gallery");
+const imageMarkup = createImageMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createImageMarkup(items) {
+  return items
+    .map((item) => {
+      return `
+  <div class="gallery__item">
+     <a class="gallery__link" href="${item.original}">
+         <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}" />
+     </a>
+  </div>
+  `;
+    })
+    .join("");
+}
+
+console.log(imageMarkup);
